458c410254c37ab7660ed91b803f3d3648aaac44
Tom Rochette <roctom@gmail.com>
2016-08-31 22:26:53 -0400

    Add a script to generate new articles based on the article template.

---
+++ agi/new-article.php
@@ -0,0 +1,20 @@
+<?php
+
+// Get paper title
+$title = $argv[1];
+$folder = preg_replace('/[ -\.:]+/', '-', strtolower($title));
+$folderPath = $folder;
+$articlePath = $folderPath.'/article.md';
+
+// Confirm
+
+// Generate folder
+mkdir($folderPath);
+// Copy template.md to article.md
+copy('template.md', $articlePath);
+// Update the article title and date
+$content = file_get_contents($articlePath);
+$content = str_replace('Template', $title, $content);
+$content = str_replace('2016-01-01', date('Y-m-d'), $content);
+file_put_contents($articlePath, $content);
+echo 'Created '.$articlePath.PHP_EOL;
