5d3a4d62155724fbfd2dece7f04a0ba667afd344
Tom Rochette <roctom@gmail.com>
2016-09-06 23:47:34 -0400

    Initial new-paper.php script based on new-article.php which helps with the generation of new paper articles by inserting text in the relevant documents.

---
+++ agi/new-paper.php
@@ -0,0 +1,44 @@
+<?php
+
+if (count($argv) !== 4) {
+	echo 'php '.$argv[0].' author title year'.PHP_EOL;
+	exit;
+}
+
+// Get author name
+$author = $argv[1];
+// Get paper title
+$title = $argv[2];
+// Get paper year
+$year = $argv[3];
+$folder = preg_replace('/[ -.:!?]+/', '-', strtolower($author.' '.$title));
+$folderPath = 'papers/'.$folder;
+$articlePath = $folderPath.'/article.md';
+
+// Confirm
+
+// Generate folder
+mkdir($folderPath);
+// Copy template.md to article.md
+copy('template.md', $articlePath);
+// Update the article title and date
+$articleTitle = $author.' - '.$title.' ('.$year.')';
+$content = file_get_contents($articlePath);
+$content = str_replace('Template', $articleTitle, $content);
+$content = str_replace('2016-01-01', date('Y-m-d'), $content);
+file_put_contents($articlePath, $content);
+echo 'Created '.$articlePath.PHP_EOL;
+
+// Append to papers/article.md
+$authorText = '## '.$author;
+$articleText = '* ['.$title.' ('.$year.')]('.$folder.')';
+$content = PHP_EOL.PHP_EOL.$authorText.PHP_EOL.$articleText;
+file_put_contents('papers/article.md', $content, FILE_APPEND);
+echo 'Updated papers/article.md'.PHP_EOL;
+
+// Append to my-path-to-agi/article.md
+$articleText = '### '.$title.' ('.date('Y-m').' - ?)';
+$authorText = $author;
+$content = PHP_EOL.PHP_EOL.$articleText.PHP_EOL.$authorText;
+file_put_contents('my-path-to-agi/article.md', $content, FILE_APPEND);
+echo 'Updated my-path-to-agi/article.md'.PHP_EOL;
