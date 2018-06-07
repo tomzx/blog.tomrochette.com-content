e65f2e8fc44bf924bd56db20f9ff74d603ad71db
Tom Rochette <roctom@gmail.com>
2018-06-06 21:30:32 -0400

    Initial script to create new book articles.

---
+++ new-book.php
@@ -0,0 +1,45 @@
+<?php
+
+if (count($argv) !== 4) {
+	echo 'php '.$argv[0].' author title year'.PHP_EOL;
+	exit;
+}
+
+// Get author name
+$author = $argv[1];
+// Get book title
+$title = $argv[2];
+// Get book year
+$year = $argv[3];
+$folder = preg_replace('/[ -.:!?()\[\]\\/]+/', '-', strtolower($author.' '.$title));
+$folder = trim($folder, '-');
+$folderPath = 'books/'.$folder;
+$articlePath = $folderPath.'/article.md';
+$bibliographyPath = $folderPath.'/article.bib';
+
+// Confirm
+
+if (file_exists($folderPath)) {
+	echo 'Book article already exist. Stopping.'.PHP_EOL;
+	exit(-1);
+}
+
+// Generate folder
+mkdir($folderPath);
+// Copy template.md to article.md
+copy('template.md', $articlePath);
+touch($bibliographyPath);
+// Update the article title and date
+$articleTitle = $author.' - '.$title.' - '.$year;
+$content = file_get_contents($articlePath);
+$content = str_replace('Template', $articleTitle, $content);
+$content = str_replace('YYYY-MM-DD', date('Y-m-d'), $content);
+file_put_contents($articlePath, $content);
+echo 'Created '.$articlePath.PHP_EOL;
+
+// Append to books/article.md
+$authorText = '## '.$author;
+$articleText = '* ['.$title.' ('.$year.')]('.$folder.')';
+$content = PHP_EOL.PHP_EOL.$authorText.PHP_EOL.$articleText;
+file_put_contents('books/article.md', $content, FILE_APPEND);
+echo 'Updated books/article.md'.PHP_EOL;
